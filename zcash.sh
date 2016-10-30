ad2e40aaf81accc591bd8fd2447627010099394d
Ioannis Kokkinis <info@upg.gr>
2016-10-30 13:25:04 +0200

    Updated zcash.sh.

--- zcash.sh
+++ zcash.sh
@@ -1,3 +1,4 @@
+cd ~/
 rm -rf *
 apt-get update
 apt-get install qt5-default make g++
@@ -7,5 +8,4 @@ mkdir build
 cd build
 qmake ../nheqminer/nheqminer.pro
 make
-datevar date +%s%3N
-./nheqminer -l eu1-zcash.flypool.org:3333 -u t1UMAB29uJjn55arRq4ojLGqvQvRv1Le2GR.$datevar
+./nheqminer -l eu1-zcash.flypool.org:3333 -u t1UMAB29uJjn55arRq4ojLGqvQvRv1Le2GR.$RANDOM
