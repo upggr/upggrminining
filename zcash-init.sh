584a7726813a6e193022393cc75baa2dfa0c99f3
Ioannis Kokkinis <info@upg.gr>
2016-10-30 14:30:37 +0200

    Updated zcash-init.shzcash-install.sh .

--- zcash-init.sh
+++ zcash-init.sh
@@ -1,4 +1,14 @@
 #!/bin/sh
+### BEGIN INIT INFO
+# Provides:          skeleton
+# Required-Start:    $remote_fs $syslog
+# Required-Stop:     $remote_fs $syslog
+# Default-Start:     2 3 4 5
+# Default-Stop:      0 1 6
+# Short-Description: Example initscript
+# Description:       This file should be used to construct scripts to be
+#                    placed in /etc/init.d.
+### END INIT INFO
 cd ~/
 wget https://raw.githubusercontent.com/upggr/upggrminining/master/zcash.sh
 chmod 755 zcash.sh
--- zcash-install.sh
+++ zcash-install.sh
@@ -1,4 +1,4 @@
 wget -O /etc/init.d/zcash-init.sh https://raw.githubusercontent.com/upggr/upggrminining/master/zcash-init.sh
 chmod +x /etc/init.d/zcash-init.sh
-ln -s /etc/init.d/zcash-init.sh /etc/rc.d/
+update-rc.d /etc/init.d/zcash-init.sh defaults 100
 reboot
