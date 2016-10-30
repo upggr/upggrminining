305ba49645c6f19cf3b032b97d6b8c7a4d2d7da3
Ioannis Kokkinis <info@upg.gr>
2016-10-30 14:46:03 +0200

    Updated zcash-install.sh.

--- zcash-install.sh
+++ zcash-install.sh
@@ -1,5 +1,5 @@
 wget -O /etc/init.d/zcash-init.sh https://raw.githubusercontent.com/upggr/upggrminining/master/zcash-init.sh
 chmod +x /etc/init.d/zcash-init.sh
 cd /etc/init.d/
-update-rc.d zcash-init.sh defaults 999
+update-rc.d zcash-init.sh 2 999
 reboot
