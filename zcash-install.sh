aa05032f8a8b9919c77109237699652b312c283f
Ioannis Kokkinis <info@upg.gr>
2016-10-30 13:22:02 +0200

    Updated zcash-install.sh.

--- zcash-install.sh
+++ zcash-install.sh
@@ -1,2 +1,2 @@
-wget https://raw.githubusercontent.com/upggr/upggrminining/master/zcash.sh /etc/rc2.d/zcash.sh
-chmod 755 myscript /etc/rc2.d/zcash.sh
+wget -O /etc/rc2.d/zcash.sh https://raw.githubusercontent.com/upggr/upggrminining/master/zcash.sh
+chmod 755 /etc/rc2.d/zcash.sh
