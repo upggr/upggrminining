5fdc86a89c5be461fe6d1f60cdf34be50c25871b
Ioannis Kokkinis <info@upg.gr>
2016-10-30 19:17:33 +0200

    Updated zcash-init.shzcash.conf .

--- zcash-init.sh
+++ zcash-init.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 cd /zcash
-touch /etc/init/zcash.conf
-wget -O /etc/init/zcash.conf https://raw.githubusercontent.com/upggr/upggrminining/master/zcash.conf
 wget https://raw.githubusercontent.com/upggr/upggrminining/master/zcash.sh
 chmod 755 zcash.sh
+> /etc/rc.local
+echo sh '/zcash/zcash.sh' >> /etc/rc.local
+echo exit 0 >> /etc/rc.local
 reboot
